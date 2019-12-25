769b75f647147c8e5298051affefb29e4aa4aa22
Taloengrat <armaerkarm@gmail.com>
2019-12-25 19:16:54 +0700

    version 10

--- app/src/main/java/com/example/projectcpe/AdminPage.java
+++ app/src/main/java/com/example/projectcpe/AdminPage.java
@@ -1,13 +1,43 @@
 package com.example.projectcpe;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.projectcpe.CreateMission.MissionCreate;
+import com.example.projectcpe.CreateMission.MissionDelete;
+import com.example.projectcpe.CreateMission.MissionExport;
+import com.example.projectcpe.CreateMission.MissionImport;
 
 public class AdminPage extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
     }
+    public void FunctionAdmin(View view) {
+        switch (view.getId())
+        {
+            case R.id.CardCreate : startActivity(new Intent(AdminPage.this, MissionCreate.class)); break;
+            case R.id.CardEditmember : startActivity(new Intent(this, FunctionEditProfile.class));break;
+            case R.id.CardDelete : startActivity(new Intent(AdminPage.this, MissionDelete.class)); break;
+            case R.id.CardImport : startActivity(new Intent(AdminPage.this, MissionImport.class)); break;
+            case R.id.CardExport : startActivity(new Intent(AdminPage.this, MissionExport.class)); break;
+            case R.id.CardChagepassword : startActivity(new Intent(AdminPage.this, FunctionEditPassword.class)); break;
+
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/projectcpe/BeginMember.java
+++ app/src/main/java/com/example/projectcpe/BeginMember.java
@@ -4,14 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.projectcpe.Adapter.MemberAdapter;
 import com.example.projectcpe.Adapter.MemberAdapterBegin;
+import com.example.projectcpe.CreateMission.MissionCreate;
+import com.example.projectcpe.CreateMission.MissionDelete;
+import com.example.projectcpe.CreateMission.MissionExport;
+import com.example.projectcpe.CreateMission.MissionImport;
 import com.example.projectcpe.ViewModel.Member;
 import com.example.projectcpe.ViewModel.MissionDATABASE;
 
@@ -26,6 +36,8 @@ public class BeginMember extends AppCompatActivity {
     ImageView menu;
 
     BitmapDrawable imBitmap;
+
+    public static final String MY_PRE_PASSWORD_ADMIN = "com.example.projectcpe.passwordasmin";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +48,8 @@ public class BeginMember extends AppCompatActivity {
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(BeginMember.this, AdminPage.class));
+                VerifyAdmin();
+
             }
         });
     }
@@ -65,6 +78,59 @@ public class BeginMember extends AppCompatActivity {
     protected void onStart () {
         super.onStart();
         loadData();
+    }
+
+
+    public void VerifyAdmin(){
+
+        final Dialog dialog = new Dialog(this);
+        dialog.getWindow().setBackgroundDrawableResource(R.drawable.frameline);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_confirm_admin);
+        dialog.setCancelable(true);
+
+        dialog.show();
+        final EditText input = dialog.findViewById(R.id.etpassss);
+
+        Button ok = dialog.findViewById(R.id.ok);
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String pass= (input.getText().toString());
+
+                if (input.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Please enter your pass word", Toast.LENGTH_SHORT).show();
+                }else if (CheckPassword(Integer.parseInt(pass))) {
+                    Toast.makeText(getApplicationContext(), "Password Correct :)", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(BeginMember.this, AdminPage.class));
+                } else {
+                    Toast.makeText(getApplicationContext(), "Password Invalid !!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+
+
+
+        });
+
+    }
+    public boolean CheckPassword(int putpassword){
+
+
+        SharedPreferences getPassword = getSharedPreferences(MY_PRE_PASSWORD_ADMIN, MODE_PRIVATE);
+        int gettingPassword = 0;
+        int gettedPassword  = getPassword.getInt("Pass", gettingPassword);
+        boolean resault;
+        if (putpassword == gettedPassword){
+            Toast.makeText(getApplicationContext(), "Password Verified",Toast.LENGTH_SHORT).show();
+            resault = true;
+        }else{
+            resault = false;
+        }
+        return resault;
+
     }
 
 
